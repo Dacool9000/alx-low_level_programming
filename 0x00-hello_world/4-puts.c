279e1e6c40299d80467efe59c9f04479ebd01c2b
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-10 11:34:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * printing using puts
+ *main - printing using puts
+ *Return: 0 (well)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
