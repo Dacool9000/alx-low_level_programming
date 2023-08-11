4fc341e2e6c1a77138a127eddee9c9d020834fd4
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-11 10:18:16 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,6 +19,6 @@ int main(void)
 	else if (mod < 6 && mod != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, mod);
 	else
-		printf("Last digit of %d is %d and is less than \n", n, mod);
+		printf("Last digit of %d is %d and is 0\n", n, mod);
 return (0);
 }
