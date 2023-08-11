29ec518e29d152c19664a68116b376ed31fdba33
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-11 11:54:22 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - good
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	for (; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+return (0);
+}
