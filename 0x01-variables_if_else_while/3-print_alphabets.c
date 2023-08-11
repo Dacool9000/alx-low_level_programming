2a15e18f1805bd96c5fb2afaffabc694e40b1d90
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-11 10:41:12 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - retry
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	)
+	while (b <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+			putchar('\n');
+	return (0);
+}
