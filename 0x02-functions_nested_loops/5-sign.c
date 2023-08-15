d588aad5ffec6794583ed01b1ae93e932fc39da0
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-15 08:45:14 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints possitves or negatives integers
+ * @n: the number to be tested
+ * Return: 1 if positve - when negative
+ */
+int print_sign(int n)
+
+{
+if (n > 0)
+{
+	_putchar(43);
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar(45);
+	return (-1);
+}
+else if (n == 0)
+{
+	_putchar(48);
+	return (0);
+}
+}
