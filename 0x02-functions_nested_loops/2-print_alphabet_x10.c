e0bb40e8ad8b58832a5e7bcee3732c55d100258c
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-15 07:08:11 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times 
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char c;
+
+	a = 1;
+	c = 'a';
+
+	while (a <= 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		c = 'a';
+		a++;
+	}
+}
