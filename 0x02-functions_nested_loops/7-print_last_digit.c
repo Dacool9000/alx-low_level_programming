4f1596d8c8382331a09bc6da3b1d6d124eaeb0d7
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-15 09:14:55 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit og a;number
+ * @c: the number to;be checked
+ * Return: 0 value
+ */
+int print_last_digit(int c)
+{
+int last;
+
+last = c % 10;
+return (last);
+}
