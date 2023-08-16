ca9e25f19bc638bea5d265af7afbf7b6672617c6
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-16 16:37:12 -0700

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - fibonici
+ * Return: 0
+ */
+int main (void)
+{
+	int a, i, fib, c;
+
+	a = 1;
+	fib = i = 0;
+
+	for (c = 1; c <= 50; c++)
+	{
+		fib = i + a;
+		i = a;
+		a = fib;
+		printf("%d, ", fib);
+	}
+}
