be2ff5c41b0fc199ed98110c03b0cec21d287bc6
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-16 12:49:00 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - thisvprogram adds two integers
+ * @a: integer 1
+ * @c: interger 2
+ * Return: the result of the addition is passed as an argument
+ */
+int add(int a, int c)
+{
+	int x;
+
+	x = 0;
+
+	x = a + c;
+
+	return (x);
+}
