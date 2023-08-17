d20e5cd5b06f4b4b9ae2c46312f393e30ee215bf
oriyomi <oriyomioluwasegun9000@gmail.com>
2023-08-16 17:17:42 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - fibonici
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long a, i, fib;
+	int c;
+	unsigned long fib2 = 2;
+
+	a = 2;
+	fib = i = 1;
+	printf("2");
+	for (c = 1; c <= 49; c++)
+	{
+		fib = i + a;
+		i = a;
+		a = fib;
+		if ((fib % 2 == 0) && fib <= 4000000)
+		{
+			fib2 = fib + fib2;
+		printf(", ");
+		printf("%lu", fib2);
+		}
+	}
+	printf("\n");
+	return (0);
+}
